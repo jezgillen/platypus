9922ff3d9eecc2774650152415315d45d81ae2ca
Jeremy Gillen <jez.gillen@gmail.com>
2017-09-15 15:32:41 +1000

    removed some stuff from hash.py

--- hash.py
+++ hash.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
 import sys, string
-from itertools import chain, product
-
-def bruteforce(charset, maxlength):
-    return (''.join(candidate)
-        for candidate in chain.from_iterable(product(charset, repeat=i)
-        for i in range(1, maxlength + 1)))
-
 
 def myhash(line):
     acc = 0
@@ -19,13 +12,3 @@ def myhash(line):
 
 for line in sys.stdin:
     print(myhash(line))
-
-#i = 0
-#for attempt in bruteforce(string.ascii_lowercase, 5):
-#    print(i,end='\r')
-#    if(myhash("aaaaa"+attempt) == 1616977757):
-#        print("yay")
-#        print(attempt)
-#        exit()
-#    i += 1
-
